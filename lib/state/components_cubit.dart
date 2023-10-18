f5089782fc63c37b2dbe5b72be6890d3980b8134
A Arif A S <arifauliasutarman@gmail.com>
2023-10-18 09:43:51 +0700

    chore: formatting

--- lib/state/components_cubit.dart
+++ lib/state/components_cubit.dart
@@ -10,20 +10,23 @@ class ComponentsCubit extends Cubit<List<ComponentData>> {
 
   void add(ComponentData component) => emit([...state, component]);
 
-  void remove(int index) => emit(state.whereIndexed((index, element) => index != index).toList());
+  void remove(int index) =>
+      emit(state.whereIndexed((index, element) => index != index).toList());
 
   void clear() => emit(List.empty());
 
   bool contains(int index) => state.contains(index);
 
   void reorder(int oldIndex, int newIndex) {
-    if (oldIndex < 0 || oldIndex >= state.length || newIndex < 0 || newIndex >= state.length) {
+    if (oldIndex < 0 ||
+        oldIndex >= state.length ||
+        newIndex < 0 ||
+        newIndex >= state.length) {
       // TODO(damywise): Handle this better
       return;
     }
 
-    final components = List<ComponentData>.of(state)
-      ..swap(oldIndex, newIndex);
+    final components = List<ComponentData>.of(state)..swap(oldIndex, newIndex);
 
     emit(components);
   }
