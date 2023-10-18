bf430d64a5f68a43aa81ebd04ecb9c580d7ab87f
A Arif A S <arifauliasutarman@gmail.com>
2023-10-18 09:40:10 +0700

    refactor: components_bloc->cubit

---
+++ lib/state/components_cubit.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: public_member_api_docs
+
+import 'package:collection/collection.dart';
+import 'package:editicert/models/component_data.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ComponentsCubit extends Cubit<List<ComponentData>> {
+  ComponentsCubit(super.initialState);
+
+  void add(ComponentData component) => emit([...state, component]);
+
+  void remove(int index) => emit(state.whereIndexed((index, element) => index != index).toList());
+
+  void clear() => emit(List.empty());
+
+  bool contains(int index) => state.contains(index);
+
+  void reorder(int oldIndex, int newIndex) {
+    if (oldIndex < 0 || oldIndex >= state.length || newIndex < 0 || newIndex >= state.length) {
+      // TODO(damywise): Handle this better
+      return;
+    }
+
+    final components = List<ComponentData>.of(state)
+      ..swap(oldIndex, newIndex);
+
+    emit(components);
+  }
+}
