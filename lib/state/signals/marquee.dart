3ec5238eac94306e09389ead85cd9f9732e2497c
A Arif A S <arifauliasutarman@gmail.com>
2024-01-07 13:41:27 +0700

    feat: (maybe) improve marqueeRect performance

--- lib/state/signals/marquee.dart
+++ lib/state/signals/marquee.dart
@@ -27,61 +27,67 @@ final marqueeRect = computed(() {
     (rect.bottomLeft.toVector2(), rect.topLeft.toVector2()),
   ].map((e) => (e.$1, e.$2)).toList();
 
-  batch(() {
-    for (final element in canvasElements()) {
-      final linesElement = [
-        (
-          element.transform.rotated.offset0.toVector2(),
-          element.transform.rotated.offset1.toVector2(),
-        ),
-        (
-          element.transform.rotated.offset1.toVector2(),
-          element.transform.rotated.offset2.toVector2(),
-        ),
-        (
-          element.transform.rotated.offset2.toVector2(),
-          element.transform.rotated.offset3.toVector2(),
-        ),
-        (
-          element.transform.rotated.offset3.toVector2(),
-          element.transform.rotated.offset0.toVector2(),
-        ),
-      ]
-          .map(
-            (e) => (
-              fromScene(e.$1.toOffset()).toVector2(),
-              fromScene(e.$2.toOffset()).toVector2(),
-            ),
-          )
-          .toList();
+  final ids = <String>[];
+  for (final element in canvasElements()) {
+    final linesElement = [
+      (
+        element.transform.rotated.offset0.toVector2(),
+        element.transform.rotated.offset1.toVector2(),
+      ),
+      (
+        element.transform.rotated.offset1.toVector2(),
+        element.transform.rotated.offset2.toVector2(),
+      ),
+      (
+        element.transform.rotated.offset2.toVector2(),
+        element.transform.rotated.offset3.toVector2(),
+      ),
+      (
+        element.transform.rotated.offset3.toVector2(),
+        element.transform.rotated.offset0.toVector2(),
+      ),
+    ]
+        .map(
+          (e) => (
+            fromScene(e.$1.toOffset()).toVector2(),
+            fromScene(e.$2.toOffset()).toVector2(),
+          ),
+        )
+        .toList();
 
-      var intersect = false;
-      var offsetInside = 0;
-
-      for (final element1 in linesElement) {
-        if (intersect) continue;
-        for (final element2 in linesRect) {
-          if (intersect) continue;
-          intersect = isTwoLinesIntersetcing(
-            element1.$1,
-            element1.$2,
-            element2.$1,
-            element2.$2,
-          );
-        }
+    var offsetInside = 0;
+    element.transform.rotated.offsets.map(fromScene).forEach((e) {
+      if (rect.contains(e)) {
+        offsetInside++;
       }
+    });
 
-      element.transform.rotated.offsets.map(fromScene).forEach((e) {
-        if (rect.contains(e)) {
-          offsetInside++;
-        }
-      });
+    if (offsetInside == 4) {
+      ids.add(element.id);
+      break;
+    }
 
-      //TODO: change 4 to number of polygon
-      if (intersect || offsetInside == 4) {
-        canvasHoveredMultipleElements.peek().add(element.id);
+    var intersect = false;
+    for (final element1 in linesElement) {
+      if (intersect) continue;
+      for (final element2 in linesRect) {
+        if (intersect) continue;
+        intersect = isTwoLinesIntersetcing(
+          element1.$1,
+          element1.$2,
+          element2.$1,
+          element2.$2,
+        );
       }
     }
-  });
+
+    if (intersect) {
+      ids.add(element.id);
+      break;
+    }
+  }
+  if (ids.isNotEmpty) {
+    untracked(() => canvasHoveredMultipleElements.value = ids.toSet());
+  }
   return rect;
 });
